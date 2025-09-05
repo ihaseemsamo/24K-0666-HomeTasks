684699dda7002758fa255175195a3500c903fb59
ihaseemsamo <haseemishere@gmail.com>
2025-09-05 10:11:57 +0500

    Update Task3.cpp

--- Lab 1 /Task3.cpp
+++ Lab 1 /Task3.cpp
@@ -1,1 +1,53 @@
+#include <iostream>
+using namespace std;
 
+class Exam
+{
+public:
+    int *marks;
+    int no_of_marks;
+
+    Exam() : no_of_marks(0), marks(nullptr) {};
+    Exam(int n) : no_of_marks(n)
+    {
+        marks = new int[n];
+        input();
+    }
+
+    void input()
+    {
+        for (int i = 0; i < no_of_marks; i++)
+        {
+            cout << "Enter marks " << i + 1 << ":";
+            cin >> marks[i];
+        }
+    }
+
+    void display()
+    {
+        for (int i = 0; i < no_of_marks; i++)
+        {
+            cout << "Marks " << i << ": " << marks[i] << endl;
+        }
+    }
+};
+
+int main()
+{
+    Exam e1(3);
+    Exam e2(e1);
+
+    cout << "Exam 1"<< endl;
+    e1.display();
+    cout << "Exam 2"<< endl;
+    e2.display();
+
+    e2.marks[0] = 40;
+
+    cout << "Exam 1" << endl;
+    e1.display();
+    cout << "Exam 2"<< endl;
+    e2.display();
+
+    return 0;
+}
