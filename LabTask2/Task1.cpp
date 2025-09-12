c9d33b57224ff967ff114bb6483abe9ee517c0a7
ihaseemsamo <haseemishere@gmail.com>
2025-09-12 14:34:32 +0500

    Update Task1.cpp

--- LabTask2/Task1.cpp
+++ LabTask2/Task1.cpp
@@ -1,1 +1,70 @@
+#include <iostream>
+using namespace std;
+
+class DynamicArray {
+private:
+    int* arr;
+    int size;
+    int capacity;
+
+public:
+    DynamicArray(int initialCapacity = 4) {
+        capacity = initialCapacity;
+        size = 0;
+        arr = new int[capacity];
+    }
+
+    ~DynamicArray() {
+        delete[] arr;
+    }
+
+    void push_back(int value) {
+        if (size == capacity) {
+            resize();
+        }
+        arr[size] = value;
+        size++;
+    }
+
+    void resize() {
+        capacity *= 2;
+        int* newArr = new int[capacity];
+        for (int i = 0; i < size; i++) {
+            newArr[i] = arr[i];
+        }
+        delete[] arr;
+        arr = newArr;
+    }
+
+    void print() const {
+        for (int i = 0; i < size; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+    }
+
+    int getSize() const {
+        return size;
+    }
+
+    int getCapacity() const {
+        return capacity;
+    }
+};
+
+int main() {
+    DynamicArray arr;
+
+    arr.push_back(10);
+    arr.push_back(20);
+    arr.push_back(30);
+    arr.push_back(40);
+    arr.push_back(50);
+    arr.push_back(60);
+
+    cout << "Array elements: ";
+    arr.print();
+
+    return 0;
+}
 
