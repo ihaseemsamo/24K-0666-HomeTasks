7ace7470686c2e1169293b2b8eb8874f5a198caa
ihaseemsamo <haseemishere@gmail.com>
2025-09-19 02:35:36 -0700

    Update Task3.cpp

--- LabTask 4/Task3.cpp
+++ LabTask 4/Task3.cpp
@@ -1,1 +1,44 @@
+#include <iostream>
+using namespace std;
+
+void shellSort(int arr[], int n) {
+    for (int gap = n / 2; gap > 0; gap /= 2) {
+        for (int i = gap; i < n; i++) {
+            int temp = arr[i];
+            int j = i;
+            while (j >= gap && arr[j - gap] > temp) {
+                arr[j] = arr[j - gap];
+                j -= gap;
+            }
+            arr[j] = temp;
+        }
+    }
+}
+
+void displayArray(int arr[], int n) {
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+int main() {
+    int n;
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter " << n << " elements:\n";
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    cout << "Original Array:\n";
+    displayArray(arr, n);
+
+    shellSort(arr, n);
+
+    cout << "Sorted Array using Shell Sort:\n";
+    displayArray(arr, n);
+
+    return 0;
+}
 
